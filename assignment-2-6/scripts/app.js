371f413874f345ef3d5c53cb443e185140effc3f
Malec24 <marguerite.malec@gmail.com>
2022-09-08 17:24:03 -0500

    assignment 2-6

---
+++ assignment-2-6/scripts/app.js
@@ -0,0 +1,23 @@
+
+
+var firstNumber = 100 ; // 
+
+var secondNumber = 200 ; 
+
+console.log(firstNumber + secondNumber);
+
+const name = "Marguerite"; //
+
+
+const x = 10;
+// Here x is 10
+
+{
+const x = 2;
+// Here x is 2
+}
+
+// Here x is 10
+
+console.log("anything honestly");
+
