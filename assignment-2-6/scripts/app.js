ddb10d2e0d3c3e7995c63a7fa501e56bca5cee2c
Malec24 <marguerite.malec@gmail.com>
2022-09-18 00:51:16 -0500

    new attempt

--- assignment-2-6/scripts/app.js
+++ assignment-2-6/scripts/app.js
@@ -1,4 +1,8 @@
 
+function myFunction() {
+    alert("Hello World!");
+  }
+
 
 var firstNumber = 100 ; // 
 
@@ -12,6 +16,7 @@ const name = "Marguerite"; //
 const car = {type:"Fiat", model:"500", color:"white"};
 
 
+
 const x = 10;
 // Here x is 10
 
@@ -21,6 +26,16 @@ const x = 2;
 }
 
 // Here x is 10
+
+const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
 
 console.log("anything honestly");
 
