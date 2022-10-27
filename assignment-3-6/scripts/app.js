fb64f850e4c018223b60c9106642e7973ccfec7b
Malec24 <marguerite.malec@gmail.com>
2022-10-27 15:22:51 -0500

    sample

--- assignment-3-6/scripts/app.js
+++ assignment-3-6/scripts/app.js
@@ -13,7 +13,7 @@ function addNumbers(firstNumber, secondNumber){
 
 }
 
-
+addNumbers();
 
 
 function myFunction() {
---
+++ sample.js
@@ -0,0 +1,59 @@
+var firstName = "Mary";
+// This is a string variable type
+// A string variable has the value surrounded by quotes 
+// Use this when needing to display or store text
+
+
+var age = 32; 
+// This is a integer variable type
+// A integer variable has a value of a number with no quotes 
+// Use this when needing to display or do something with a number IE math (+ or -)
+
+var isParent = true;
+// This is a boolean variable type
+// A boolean variable either has a value of true or false with no quotes 
+//Use this when needing to identify something true or false 
+
+
+var countries = ["US","MX","AU"];
+// This is an array variable type 
+// An array variable has a value of multiple values
+// Values are inside of a square bracket
+// Each value is separated by a comma 
+// Use this when needing to store multiple of the same values
+
+var person = {
+    firstName: "Mary", 
+    age: 32,
+    isParent: true
+}
+// This is an object variable type
+// An object variable has a value of multiple properties
+// Properties are inside of a curly brace
+// Each property has a value set after a colon :  
+// Each property is separated by a comma
+// Use this when needing to store different values for a specific item IE product specification 
+
+var day = "15"; 
+var day = 15;
+
+var days = [3,10,15];
+var days = ["3","10","15"];
+var isToday = false;
+
+
+var today = {day:"Thursday",dayNumber:27}
+
+
+var dogBreed = "border collie";
+
+var goals = 2;
+
+var array1 = [1,2];
+
+var laptop = {
+   keyboard: true,
+   screen: "13 inches",
+   camera: true,
+}
+
