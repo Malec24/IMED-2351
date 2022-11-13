f16d80f632c613c56e048983ea70823f20157476
Malec24 <marguerite.malec@gmail.com>
2022-11-13 16:27:45 -0600

    updated

--- assignment-4-6/scripts/app.js
+++ assignment-4-6/scripts/app.js
@@ -1,49 +1,64 @@
 
 
-
-let favFruit = 'kiwi';
-console.log('My favorite fruit is  ' + favFruit + '.'); 
-
-
-
-if
-(x 
->
-y) {
- alert("Hello World");
-} 
-else
-{
- alert("Goodbye");
-}
-
-
-
 let txt = "Hello World";
-txt = 
-txt.toUpperCase()
-;
-
-
-
-
-switch
-(fruits) {
-  
-case
- "Banana":
-    alert("Hello")
-    break;
-  
-case
- "Apple":
-    alert("Welcome")
-    break;    
-}
+txt = txt.
+replace
+("Hello", "Welcome");
 
 
 
 Number(new Date("2022-09-30"));
+
+
+
+const age = 31;
+
+if (age > 21) {
+    console.log("You are of legal age");
+} 
+else { 
+    console.log("You are not of legal age");
+}
+
+
+
+let favFruit = 'kiwi';
+console.log('My favorite fruit is ' + favFruit + '.'); 
+
+
+
+
+let fruit = 'Apple';
+switch(fruit) {
+    case 'Apple':
+    case 'Mango':
+    case 'Pineapple':
+        console.log(`${fruit} is a fruit.`);
+        break;
+    default:
+        console.log(`${fruit} is not a fruit.`);
+        break;
+}
+
+
+
+switch ("no instrument") {
+    case "trumpet":
+      console.log("I play the trumpet");
+      break;
+    case "flute":
+      console.log("I play the flute");
+      break;
+    case "oboe":
+      console.log("I play the oboe");
+      break;
+    default:
+      console.log("I don't play an instrument. Sorry");
+      break;
+  }
+
+
+
 
 
 
